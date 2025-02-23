71eac93ea9276c216ff1180bf4241d4d20700ccd
tduc01234@gmail.com <tduc01234@gmail.com>
2025-02-23 18:30:10 +0700

    b

--- ToDoApp/Migrations/20250222041521_InitDatabase.Designer.cs
+++ ToDoApp/Migrations/20250222041521_InitDatabase.Designer.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using ToDoApp.Infrastructure;
+using ToDoApp.Infrastructures;
 
 #nullable disable
 
--- ToDoApp/Migrations/ApplicationDBContextModelSnapshot.cs
+++ ToDoApp/Migrations/ApplicationDBContextModelSnapshot.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using ToDoApp.Infrastructure;
+using ToDoApp.Infrastructures;
 
 #nullable disable
 
